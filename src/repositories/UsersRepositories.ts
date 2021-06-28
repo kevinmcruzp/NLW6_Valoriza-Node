3b925f3659723af209ef19e99c7763ab7f44b394
Kevin Cruz <kevin_mcp@outlook.com>
2021-06-28 00:21:22 -0400

    Service create user done

--- src/repositories/UsersRepositories.ts
+++ src/repositories/UsersRepositories.ts
@@ -2,6 +2,9 @@ import { EntityRepository, Repository } from "typeorm";
 import { User } from "../entities/User";
 
 @EntityRepository(User)
-class UsersRepositories extends Repository<User> {}
+class UsersRepositories extends Repository<User> {
+  
+
+}
 
 export { UsersRepositories };
